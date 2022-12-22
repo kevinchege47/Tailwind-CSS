118e57e2aec56741c4720eca1488310b51dde9fa
kevinchege47 <chegekevin001@gmail.com>
2022-12-22 17:34:57 +0300

    Added Custom Colors

--- tailwind-default.config.js
+++ tailwind-default.config.js
@@ -3,13 +3,7 @@ module.exports = {
   content: ["./dist/*.{html,js,css}"],
   theme: {
     extend: {
-      colors:{
-        primary:"#FF6363",
-        secondary:{
-          100:'#E2E2D5',
-          200:'888883',
-        }
-      }
+   
     },
   },
   plugins: [],
